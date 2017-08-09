99df615342592b8e3b347f049a46f81305571242
daniel <dtpontesinfo@gmail.com>
2017-08-09 15:33:23 -0300

    Aula 09

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url : string = 'http://loiane.com';
+
+  getValor()
+  {
+    return 1;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
